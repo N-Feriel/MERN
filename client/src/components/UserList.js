48ace4973ac280720ebb38c65351892668720402
Feriel Nabet <fnabet@ipconnex.com>
2021-05-17 21:53:33 -0400

    add userlist to frontEnd

---
+++ client/src/components/UserList.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+function UserList() {
+  const [usersList, setUserList] = useState([]);
+
+  const getUsersList = async () => {
+    try {
+      const response = await fetch("/api/users", {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Accept-Charset": "utf-8",
+        },
+      });
+
+      const responseBody = await response.json();
+
+      if (responseBody.status === 200) {
+        setUserList(responseBody.data);
+      } else {
+        throw responseBody.message;
+      }
+    } catch (err) {
+      console.log(err, "error");
+    }
+  };
+  useEffect(() => {
+    getUsersList();
+  }, []);
+
+  return (
+    <div>
+      <h1>Users list</h1>
+      {usersList && (
+        <ul>
+          {usersList.map((user) => (
+            <li key={user._id}>{user.email}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default UserList;
