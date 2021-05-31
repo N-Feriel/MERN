e28f9b058601b833c36ee08f4e88597a132d7fb1
Feriel Nabet <fnabet@ipconnex.com>
2021-05-31 17:00:39 -0400

    add push to admin page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useHistory } from "react-router";
 import Footer from "../components/footer/Footer";
 
 function Home() {
+  const history = useHistory();
   return (
     <div className="m-auto bg-yellow-100 opacity-60">
       <div className="pt-1 ml-6">
@@ -10,7 +12,10 @@ function Home() {
         </h1>
         <h2>Please Select user type to login </h2>
         <div className="flex flex-col space-x-3 ">
-          <button className="flex items-center justify-center text-white bg-blue-500 rounded-md ">
+          <button
+            onClick={() => history.push("/admin")}
+            className="flex items-center justify-center text-white bg-blue-500 rounded-md "
+          >
             Admin
           </button>
           <button className="flex items-center justify-center rounded-md">
